eb36fd224716947c89173b2c3d26c4043990ab8e
JaehoLee <jaeholee83@gmail.com>
2023-05-18 15:50:54 +0900

    commit

--- AIScreening/urls.py
+++ AIScreening/urls.py
@@ -19,10 +19,13 @@ from django.urls import path
 from tutorial_web.views import index, survey
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.urls import re_path as url
+from django.views.static import serve
 
 urlpatterns = [
     path('', index),
     path('survey/', survey),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
